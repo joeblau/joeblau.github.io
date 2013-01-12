1055e37e999464cb68d14378e52f44d904611335
Joe Blau <josephblau@gmail.com>
2013-01-11 21:04:15 -0800

    Upadting site design

---
+++ assets/joeblau/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: josephblau
+ * Date: 1/11/13
+ * Time: 12:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function(){
+  $('#sopaCaorusel').carousel({
+    interval: false
+  });
+  $('#mftranslatorCaorusel').carousel({
+    interval: false
+  });
+  $('#vroomtrapCaorusel').carousel({
+    interval: false
+  });
+  $('#metmoCaorusel').carousel({
+    interval: false
+  });
+
+});
+
+
+
